d4e37541a39523891364339e385479bc048c65c2
LordGift <jarupath.j@cdg.co.th>
2021-05-19 16:48:09 +0700

    extract variables

--- src/main/java/th/in/lordgift/Hbm2JavaDemo.java
+++ src/main/java/th/in/lordgift/Hbm2JavaDemo.java
@@ -16,11 +16,16 @@ import th.in.lordgift.exporter.Hbm2JavaExporterTaskHelper;
  */
 public class Hbm2JavaDemo {
 
+    private static final String hibernateCfgXml     = "/home/sunilp/IdeaProjects/Hbm2javaCodeGeneration/src/main/resources/configuration/hibernate.cfg.xml";
+    private static final String outputDirectory     = "/home/sunilp";
+    private static final String outputPackage       = "com.hrdb";
+
+
     public static void main(String[] args) {
 
         try {
 
-            File configFile = new File("/home/sunilp/IdeaProjects/Hbm2javaCodeGeneration/src/main/resources/configuration/hibernate.cfg.xml");
+            File configFile = new File(hibernateCfgXml);
             Configuration configuration = getJDBCConfiguration(configFile);
             ConfigurationAwareToolTask configurationAwareToolTask = new ConfigurationAwareToolTask(configuration);
             MetaDataDialect metaDataDialect = ((ExtendedJDBCMetaDataConfigration) configuration).getConfiguredMetaDataDialect();
@@ -28,7 +33,7 @@ public class Hbm2JavaDemo {
             Hbm2JavaExporterTask hbm2javaExporter = new Hbm2JavaExporterTaskHelper(configurationAwareToolTask, metaDataDialect);
             hbm2javaExporter.setEjb3(true);
             hbm2javaExporter.setJdk5(true);
-            hbm2javaExporter.setDestdir(new File("/home/sunilp"));
+            hbm2javaExporter.setDestdir(new File(outputDirectory));
             hbm2javaExporter.execute();
 
             System.out.println("Successfully code generated...");
@@ -43,7 +48,7 @@ public class Hbm2JavaDemo {
     public static Configuration getJDBCConfiguration(File configFile)
     {
         ExtendedJDBCConfigurationTask jdbcConfiguration = new ExtendedJDBCConfigurationTask(configFile);
-        jdbcConfiguration.setPackageName("com.hrdb");
+        jdbcConfiguration.setPackageName(outputPackage);
         jdbcConfiguration.setDetectOptimisticLock(false);
         jdbcConfiguration.setReverseStrategy("org.hibernate.cfg.reveng.DelegatingReverseEngineeringStrategy");
         return jdbcConfiguration.getConfiguration();
